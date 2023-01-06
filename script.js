ea680dfb97ff4a10abcf63982b4ab9427fd89f40
Glitch (glitch-blank-website) <none>
2023-01-06 19:31:23 +0000

    🌯🐏 Checkpoint ./index.html:39955708/1209 ./script.js:39955708/471

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-/* If you're feeling fancy you can add interactivity 
-    to your site with Javascript */
+alert("coming soon!");
 
-console.log("Hello, world!");
+const game_canvas = document.getElementById("game_canvas");
+const gl = game_canvas.getContext("webgl");
+if(gl == null) {
+  alert("you have to update your br");
+}
