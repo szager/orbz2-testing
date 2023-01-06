dd81e622cfa9d7c3e9d57145623e61e99a71b682
Glitch (glitch-blank-website) <none>
2023-01-06 20:08:48 +0000

    👵🌘 Checkpoint ./script.js:39955708/731 ./index.html:39955708/485

--- script.js
+++ script.js
@@ -5,3 +5,18 @@ const gl = game_canvas.getContext("webgl");
 if(gl == null) {
   alert("you have to update your browser because it is really old and I can't do 3D games on it because it's so old");
 }
+gl.clearColor(0.0, 0.0, 0.0, 1.0);
+gl.clear(gl.COLOR_BUFFER_BIT);
+
+game_canvas.requestFullscreen();
+
+function resizeHandler () {
+  game_canvas.width = window.innerWidth;
+  game_canvas.height = window.innerHeight;
+}
+
+function keydownHandler (e) {
+}
+
+window.onresize = resizeHandler;
+window.onkeydown = keydownHandler;
