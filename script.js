d2552f89a49820ba6907500518e38e79ca998145
Glitch (glitch-blank-website) <none>
2023-01-06 19:33:46 +0000

    🎯📌 Checkpoint ./script.js:39955708/98

--- script.js
+++ script.js
@@ -3,5 +3,5 @@ alert("coming soon!");
 const game_canvas = document.getElementById("game_canvas");
 const gl = game_canvas.getContext("webgl");
 if(gl == null) {
-  alert("you have to update your br");
+  alert("you have to update your browser because it is really old and I can't do 3D games on it because it's so old");
 }
