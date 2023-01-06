09f658afef2499856380cfc8b3ce85f5b7956272
Glitch (glitch-blank-website) <none>
2023-01-06 20:51:42 +0000

    🌴☄️ Checkpoint ./script.js:39955708/222

--- script.js
+++ script.js
@@ -7,9 +7,14 @@ var vsource = //V  S  A  U  C  E
   attribute vec4 position;
   uniform mat4 camera_matrix;
   uniform mat4 scene_matrix;
+  void main(void) {
+    gl_position = camera_matrix * scene_matrix * position;
+  }
 `
 var fsource = `
-  
+  void main(void) {
+    gl_FragColor = vec4(gl_FragCoord.xy,1.0,1.0);
+  }
 `
 if(gl == null) {
   alert("you have to update your browser because it is really old and I can't do 3D games on it because it's so old");
