0258aff5ef2747fc814e8bc246953c2271ac90f5
Glitch (glitch-blank-website) <none>
2023-01-06 21:00:37 +0000

    🕶🖐 Checkpoint ./script.js:39955708/889

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ alert("coming soon!");
 
 const game_canvas = document.getElementById("game_canvas");
 const gl = game_canvas.getContext("webgl");
+var aspect_ratio = 1.1;
 var vsource = //V  S  A  U  C  E
 `
   attribute vec4 position;
@@ -17,17 +18,19 @@ var fsource = `
   }
 `
 if(gl == null) {
-  alert("you have to update your browser because it is really old and I can't do 3D games on it because it's so old");
+  alert("this web browser is really old and you need to use a newer browser because your browser can't do webgl which means no 3d games on the web lol");
 }
 gl.clearColor(0.0, 0.0, 0.0, 1.0);
 gl.clear(gl.COLOR_BUFFER_BIT);
 
-
-
+function draw_scene() {
+  
+}
 
 function resizeHandler () {
   game_canvas.width = window.innerWidth;
   game_canvas.height = window.innerHeight;
+  aspect_ratio = window.innerWidth / window.innerHeight;
 }
 
 window.onresize = resizeHandler;
