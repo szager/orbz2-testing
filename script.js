e237af4dc521bf12d9c725f2e1d19c6de9fd94eb
Glitch (glitch-blank-website) <none>
2023-01-09 19:29:02 +0000

    🚩🌨 Checkpoint ./script.js:39955708/256

--- script.js
+++ script.js
@@ -2,6 +2,14 @@ alert("coming soon!");
 
 const game_canvas = document.getElementById("game_canvas");
 const gl = game_canvas.getContext("webgl");
+var vs_source = `
+  attribute vec4 position;
+  uniform mat4 scene_matrix;
+  uniform mat4 camera_matrix;
+  void main(void) {
+    gl_position = position * scene_matrix * camera_matrix;
+  }
+`;
 
 var aspect_ratio = 1;
 
