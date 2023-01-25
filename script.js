03f4c7e6053ec32c54584d28bdc673dcf4911ac5
Glitch (glitch-blank-website) <none>
2023-01-25 19:31:41 +0000

    🌋💁 Checkpoint ./script.js:39955708/110

--- script.js
+++ script.js
@@ -192,19 +192,19 @@ var vertex_count = positions.length / 3;
 
 
 var light_directions = [
-  0.1, 0.2, 2.0,
-  -0.1, -0.2, -2.0,
-  -0.1, -0.1, -0.2,
-  0.4, 0.1, -0.1,
+  0.1, -0.5, 0.5,
+  -0.1, 0.5, -0.5,
+  0.8, -0.3, -0.2,
+  -0.4, -0.1, -1.0,
 ]
 
 normalize(light_directions);
 
 var light_colors = [
-  0.8, 0.8, 0.8,
-  0.15, 0.15, 0.15,
-  0.8, 0.2, 0.0,
-  0.0, 0.1, 0.7,
+  0.9, 0.9, 0.9,
+  0.4, 0.4, 0.4,
+  0.8, 0.0, 0.7,
+  0.0, 0.6, 0.9,
 ]
 
 var float32_object_colors = new Float32Array(object_colors);
