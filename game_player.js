237ab8969e24667d6d6ee45653a8d985bd4ba618
Glitch (orbeez-2) <none>
2023-01-28 22:05:11 +0000

    ⛷🚖 Checkpoint ./model.js:39955708/192 ./game_player.js:39955708/637

--- game_player.js
+++ game_player.js
@@ -3,6 +3,24 @@ import {game} from "./game.js";
 class game_player {
   constructor(canvas) {
     this.game = new game(canvas);
+    this.stopping = false;
+    this.paused = false;
+    this.bound_update_method = this.update.bind(this);
+  }
+  pause() {
+    this.paused = true;
+  }
+  unpause() {
+    this.paused = false;
+  }
+  stop() {
+    this.stopping = true;
+  }
+  update() {//but i can't make it private
+    if(!this.paused) {
+      this.game.update();
+    }
+    requestAnimationFrame(this.bound_update_method);
   }
 }
 
---
+++ model.js
@@ -0,0 +1,7 @@
+class model {
+  constructor(positions, normals, faces) {
+    this.positions = positions;
+    this.normals = normals;
+    this.faces = faces;
+  }
+}
