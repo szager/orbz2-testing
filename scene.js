b3522c3e62787aa816fc67382c5bac044c47ed1d
Glitch (orbeez-2) <none>
2023-01-30 16:43:18 +0000

    🏰🚩 Checkpoint ./scene.js:39955708/653

--- scene.js
+++ scene.js
@@ -2,9 +2,19 @@ class scene {
   constructor(canvas) {
     this.canvas = canvas;
     this.gl = this.canvas.getContext("webgl");
+    this.vertex_positions = [];
+    this.faces = [];
+    this.vertex_object_indices = [];
+    this.object_translations = [];
+    this.object_colors = [];
+    this.camera_rotation = [0, 0, 0];
+    this.camera_translation = [0, 0, 0];
+  }
+  initialize_buffers() {
+    
   }
   draw() {
-    this.gl.clearColor(Math.random(), Math.random(), Math.random(), 1.0);
+    this.gl.clearColor(0.8, 0.8, 0.8, 1.0);
     this.gl.clear(this.gl.COLOR_BUFFER_BIT);
   }
 }
