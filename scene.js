844d258a5c67cd521ba42a4cf88dd0472bf3dfff
Glitch (orbeez-2) <none>
2023-01-30 17:07:06 +0000

    🚃🎷 Checkpoint ./scene.js:39955708/556

--- scene.js
+++ scene.js
@@ -1,18 +1,33 @@
 class scene {
-  constructor(canvas, vertex_shader_source, fragment_shader_source) {
+  constructor(canvas) {
     this.canvas = canvas;
     this.gl = this.canvas.getContext("webgl");
+    
+    
     this.vertex_positions = [];
-    this.faces = [];
+    
     this.vertex_object_indices = [];
+    
     this.object_translations = [];
+    
     this.object_colors = [];
+        
+    this.faces = [];
+    
+    
     this.camera_rotation = [0, 0, 0];
+    
     this.camera_translation = [0, 0, 0];
+    
+    
+    this.vertex_shader_source = `
+    `;
+    this.fragment_shader_source = `
+    `;
     this.vertex_shader = this.gl.createShader(this.gl.FRAGMENT_SHADER);
     this.fragment_shader = this.gl.createShader(this.gl.FRAGMENT_SHADER);
-    this.gl.shaderSource(this.vertex_shader, vertex_shader_source);
-    this.gl.shaderSource(this.fragment_shader, fragment_shader_source);
+    this.gl.shaderSource(this.vertex_shader, this.vertex_shader_source);
+    this.gl.shaderSource(this.fragment_shader, this.fragment_shader_source);
     this.shader_program = this.gl.createProgram();
     this.gl.attachShader(this.shader_program, this.vertex_shader);
     this.gl.attachShader(this.shader_program, this.fragment_shader);
@@ -25,13 +40,13 @@ class scene {
         object_index: this.gl.getAttribLocation(this.shader_program, "object_index"),
       },
       uniform_locations: {
-        projection_matrix: gl.getUniformLocation(shader_program, "camera_matrix"),
-        _matrix: gl.getUniformLocation(shader_program, "scene_matrix"),
-        normal_matrix: gl.getUniformLocation(shader_program, "normal_matrix"),
-        object_positions: gl.getUniformLocation(shader_program, "object_positions"),
-        object_colors: gl.getUniformLocation(shader_program, "object_colors"),
-        light_directions: gl.getUniformLocation(shader_program, "light_directions"),
-        light_colors: gl.getUniformLocation(shader_program, "light_colors")
+        perspective_matrix: this.gl.getUniformLocation(this.shader_program, "perspective_matrix"),
+        view_matrix: this.gl.getUniformLocation(this.shader_program, "view_matrix"),
+        normal_matrix: this.gl.getUniformLocation(this.shader_program, "normal_matrix"),
+        object_translations: this.gl.getUniformLocation(this.shader_program, "object_translations"),
+        object_colors: this.gl.getUniformLocation(this.shader_program, "object_colors"),
+        light_directions: this.gl.getUniformLocation(this.shader_program, "light_directions"),
+        light_colors: this.gl.getUniformLocation(this.shader_program, "light_colors")
       },
     };
   }
