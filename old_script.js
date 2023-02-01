05d398490be1d57f08036bbead94390a749fb590
Glitch (orbeez-2) <none>
2023-02-01 20:23:11 +0000

    🎣🎹 Checkpoint ./old_script.js:39955708/4

--- old_script.js
+++ old_script.js
@@ -274,7 +274,7 @@ var fs_source = `
       highp vec3 light_direction = light_directions[i];
       highp vec3 light_color = light_colors[i];
       highp vec3 h = normalize(normal_camera - light_direction); //🤣🤣🤣🤣🤣🤣🤣🤣🤣🤣
-      highp float fresnel = pow(max(1.0 - dot(h, -light_direction), 0.0),5.0) * 5.0 + 0.5;
+      highp float fresnel = pow(max(1.0 - dot(h, -light_direction), 0.0),5.0) * 1.0 + 0.0;
       specular_illumination += pow(max(0.0,dot(normal_normal, h)),32.0) * light_colors[i] * fresnel;
       diffuse_illumination += max(dot(normal_normal, light_direction),0.0) * light_colors[i];
     }
