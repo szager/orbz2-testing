fe548a0f78bf6a7be9962d2e12bbe86a8330c22b
Glitch (orbeez-2) <none>
2023-02-06 16:53:59 +0000

    🏞🕍 Checkpoint ./scene.js:39955708/1462 ./index.html:39955708/20

--- scene.js
+++ scene.js
@@ -4,7 +4,10 @@ class scene {
   constructor(canvas) {
     this.canvas = canvas;
     this.gl = this.canvas.getContext("webgl");
-    
+    this.fov = .8;
+    this.aspect_ratio = this.canvas.width / this.canvas.height;
+    this.min_distance = .1;
+    this.max_distance = 100;
     
     this.vertex_positions = [
       0.0, -1.0, 0.0,
@@ -39,7 +42,7 @@ class scene {
     
     this.camera_rotation = [0, 0, 0];
     
-    this.camera_translation = [0, 0, -4];
+    this.camera_translation = [0, 0, -40];
     
     
     this.vertex_shader_source = `
@@ -68,8 +71,8 @@ class scene {
         transformed_normal = normalize(normal * normal_matrix);
         mediump int int_object_index = int(object_index);
         vertex_color = object_colors[int_object_index];
-        relative_position = position + object_translations[int_object_index] - camera_translation;
-        gl_Position = camera_rotation_matrix * perspective_matrix * vec4(relative_position, 1.0);
+        relative_position = vposition + object_translations[int_object_index] - camera_translation;
+        gl_Position = vec4(relative_position, 1.0) * camera_rotation_matrix * perspective_matrix;
       }
     `;
     this.fragment_shader_source = `
@@ -159,93 +162,78 @@ class scene {
     let perspective_matrix = mat4.create();
     mat4.perspective(
       perspective_matrix,
-      fov,
-      aspect_ratio,
-      min_distance,
-      max_distance
+      this.fov,
+      this.aspect_ratio,
+      this.min_distance,
+      this.max_distance
     );
-    mat4.rotate(camera_rotation_matrix, camera_rotation_matrix, camera_rotation[0], [-1, 0, 0]);
-    mat4.rotate(camera_matrix, camera_matrix, camera_rotation[1], [0, -1, 0]);
-    mat4.rotate(camera_matrix, camera_matrix, camera_rotation[2], [0, 0, -1]);
-    mat4.translate(camera_matrix, camera_matrix, camera_translation.map(n => -n));
-    let scene_matrix = mat4.create();
-  //mat4.rotate(scene_matrix, scene_matrix, time * 0.018403, [1.0, 0.0, 0.0]);
-  //mat4.rotate(scene_matrix, scene_matrix, time * 0.023485, [0.0, 1.0, 0.0]);
-  //mat4.rotate(scene_matrix, scene_matrix, time * 0.047634, [0.0, 0.0, 1.0]);
+    mat4.rotate(camera_rotation_matrix, camera_rotation_matrix, this.camera_rotation[0], [-1, 0, 0]);
+    mat4.rotate(camera_rotation_matrix, camera_rotation_matrix, this.camera_rotation[1], [0, -1, 0]);
+    mat4.rotate(camera_rotation_matrix, camera_rotation_matrix, this.camera_rotation[2], [0, 0, -1]);
     let normal_matrix = mat3.create();
-    mat3.normalFromMat4(normal_matrix, scene_matrix);
+    mat3.normalFromMat4(normal_matrix, camera_rotation_matrix);
   
   
-    gl.bindBuffer(gl.ARRAY_BUFFER, position_buffer);
-    gl.vertexAttribPointer(
-      program_info.attribute_locations.position,
+    this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.vertex_position_buffer);
+    this.gl.vertexAttribPointer(
+      this.program_info.attribute_locations.vertex_position,
       3,
-      gl.FLOAT,
+      this.gl.FLOAT,
       false,
       0,
       0
     );
-    gl.bindBuffer(gl.ARRAY_BUFFER, normal_buffer);
-    gl.vertexAttribPointer(
-      program_info.attribute_locations.normal,
+    this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.vertex_normal_buffer);
+    this.gl.vertexAttribPointer(
+      this.program_info.attribute_locations.vertex_normal,
       3,
-      gl.FLOAT,
+      this.gl.FLOAT,
       false,
       0,
       0
     );
-    gl.bindBuffer(gl.ARRAY_BUFFER, object_index_buffer);
-    gl.vertexAttribPointer(
-      program_info.attribute_locations.object_index,
+    this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.object_index_buffer);
+    this.gl.vertexAttribPointer(
+      this.program_info.attribute_locations.object_index,
       1,
-      gl.FLOAT,
+      this.gl.FLOAT,
       false,
       0,
       0
     );
 
-    gl.enableVertexAttribArray(program_info.attribute_locations.position);
-    gl.enableVertexAttribArray(program_info.attribute_locations.normal);
-    gl.enableVertexAttribArray(program_info.attribute_locations.object_index);
-    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, face_buffer);
-    gl.useProgram(program_info.program);
+    this.gl.enableVertexAttribArray(this.program_info.attribute_locations.vertex_position);
+    this.gl.enableVertexAttribArray(this.program_info.attribute_locations.vertex_normal);
+    this.gl.enableVertexAttribArray(this.program_info.attribute_locations.object_index);
+    this.gl.bindBuffer(this.gl.ELEMENT_ARRAY_BUFFER, this.face_buffer);
+    this.gl.useProgram(this.program_info.program);
 
-    gl.uniform3fv(
-      program_info.uniform_locations.object_positions,
-      new Float32Array(object_positions)
+    this.gl.uniform3fv(
+      this.program_info.uniform_locations.object_translations,
+      new Float32Array(this.object_positions)
     );
-    gl.uniform3fv(
-      program_info.uniform_locations.object_colors,
-      float32_object_colors
-    );
-  
-  
-    gl.uniform3fv(
-      program_info.uniform_locations.light_directions,
-      float32_light_directions
-    );
-    gl.uniform3fv(
-      program_info.uniform_locations.light_colors,
-      float32_light_colors
+    this.gl.uniform3fv(
+      this.program_info.uniform_locations.object_colors,
+      this.float32_object_colors
     );
 
   
-    gl.uniformMatrix4fv(
-      program_info.uniform_locations.camera_matrix,
+    this.gl.uniformMatrix4fv(
+      this.program_info.uniform_locations.camera_matrix,
       false,
-      camera_matrix
+      perspective_matrix
     );
-    gl.uniformMatrix3fv(
-      program_info.uniform_locations.normal_matrix,
+    this.gl.uniformMatrix3fv(
+      this.program_info.uniform_locations.normal_matrix,
       false,
       normal_matrix
     );
-    gl.uniformMatrix4fv(
-      program_info.uniform_locations.scene_matrix,
+    this.gl.uniformMatrix4fv(
+      this.program_info.uniform_locations.camera_rotaion_matrix,
       false,
-      scene_matrix
+      camera_rotation_matrix
     );
-    gl.drawElements(gl.TRIANGLES, faces.length, gl.UNSIGNED_SHORT, 0);
+    this.gl.drawElements(this.gl.TRIANGLES, this.faces.length, this.gl.UNSIGNED_SHORT, 0);
     
   }
 }
