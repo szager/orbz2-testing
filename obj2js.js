0a92f402f222a793d8200b29b99149d1d7125250
Glitch (orbeez-2) <none>
2023-02-22 20:13:23 +0000

    💫🚗 Checkpoint ./obj2js.js:39955708/373

--- obj2js.js
+++ obj2js.js
@@ -1,5 +1,6 @@
 import {model} from "./model.js";
 
+//this code is not supposed to be good code. it is just a makeshift tool to help import obj models into my project.
 
 function obj2js(obj_file_contents) {
   let js_file_contents = "";
@@ -14,12 +15,18 @@ function obj2js(obj_file_contents) {
   let content = obj_file_contents.substring(obj_file_contents.indexOf("v"));
   let v_paragraph = content.substring(0, content.indexOf("vn"));
   let vn_paragraph = content.substring(content.indexOf("vn"), content.indexOf("s"));
-  let f_paragraph = content.substring(content.indexOf("f")).replaceAll("f ","").replaceAll(`
-`, "").replaceAll(" ",`
+  let f_paragraph = content.substring(content.indexOf("f")).replaceAll("f ","").replaceAll(" ",`
+`).split(`
 `);
   
+  f_paragraph.forEach(corner => {
+    if(corners.indexOf(corner) == -1 ) {
+      corners.push(corner);
+    }
+    faces.push(corners.indexOf(corner));
+  });
   
-  let json_blob = new Blob([f_paragraph], {type : "text/plain"});
+  let json_blob = new Blob([corners], {type : "text/plain"});
   let blob_url  = window.URL.createObjectURL(json_blob);
   window.location.assign(blob_url);
 }
