dcacfc300ca844ed9ce68bdf0da2ab6bf9670acb
Glitch (orbeez-2) <none>
2023-02-22 22:40:54 +0000

    ⛳️🕰 Checkpoint ./obj2js.js:39955708/1079

--- obj2js.js
+++ obj2js.js
@@ -1,6 +1,5 @@
 import {model} from "./model.js";
 
-//this code is not supposed to be good code. it is just a makeshift tool to help import obj models into my project.
 
 function obj2js(obj_file_contents) {
   let js_file_contents = "";
@@ -13,8 +12,14 @@ function obj2js(obj_file_contents) {
   let faces = [];
   
   let content = obj_file_contents.substring(obj_file_contents.indexOf("v"));
-  let v_paragraph = content.substring(0, content.indexOf("vn"));
-  let vn_paragraph = content.substring(content.indexOf("vn"), content.indexOf("s"));
+  let v_paragraph = content.substring(0, content.indexOf("vn")).replaceAll("v ","").replaceAll(" ",`
+`).split(`
+`);
+  let vn_paragraph = content.substring(content.indexOf("vn"), content.indexOf("s")).replaceAll("vn ","").replaceAll(" ",`
+`).split(`
+`);
+  
+  
   let f_paragraph = content.substring(content.indexOf("f")).replaceAll("f ","").replaceAll(" ",`
 `).split(`
 `);
@@ -22,11 +27,22 @@ function obj2js(obj_file_contents) {
   f_paragraph.forEach(corner => {
     if(corners.indexOf(corner) == -1 ) {
       corners.push(corner);
+      let pos_index = Number(corner.split("//")[0]);
+      let n_index = Number(corner.split("//")[1]);
+      positions.push(v_paragraph[pos_index]);
+      normals.push(vn_paragraph[n_index]);
     }
     faces.push(corners.indexOf(corner));
   });
   
-  let json_blob = new Blob([corners], {type : "text/plain"});
+  
+  
+  js_file_contents = `
+    import {model} from "./model.js";
+    
+    new model()
+  `;
+  let json_blob = new Blob([positions], {type : "text/plain"});
   let blob_url  = window.URL.createObjectURL(json_blob);
   window.location.assign(blob_url);
 }
