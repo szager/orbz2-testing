048de9ebe86cf94d56cdec3a44af1756ae4d7bab
Glitch (orbeez-2) <none>
2023-02-22 23:14:30 +0000

    🦂🌪 Checkpoint ./.glitch-assets:39955708/370 ./main.js:39955708/800 ./orbee_model.js:39955708/3018

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
-//import {game} from "./game.js";
+import {game} from "./game.js";
 
-//const the_game = new game(
-  //document.querySelector("canvas")
-//);
+const the_game = new game(
+  document.querySelector("canvas")
+);
 
-import {obj2js} from "./obj2js.js";
-obj2js(``);
+//import {obj2js} from "./obj2js.js";
+//fetch("https://cdn.glitch.global/c6678e5a-6e79-4893-82bc-4512fb9bd910/orbee.obj?v=1677107052951").then(response => {
+    //response.text().then(text => {
+      //obj2js(text);
+    //}
+  //)
+//});
