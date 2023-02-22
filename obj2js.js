13355bd222c5ba3eeb77d172c75e117d3ac5f570
Glitch (orbeez-2) <none>
2023-02-22 20:02:15 +0000

    🎨🦂 Checkpoint ./obj2js.js:39955708/498

--- obj2js.js
+++ obj2js.js
@@ -12,12 +12,14 @@ function obj2js(obj_file_contents) {
   let faces = [];
   
   let content = obj_file_contents.substring(obj_file_contents.indexOf("v"));
+  let v_paragraph = content.substring(0, content.indexOf("vn"));
+  let vn_paragraph = content.substring(content.indexOf("vn"), content.indexOf("s"));
+  let f_paragraph = content.substring(content.indexOf("f")).replaceAll("f ","").replaceAll(`
+`, "").replaceAll(" ",`
+`);
   
   
-  
-  
-  
-  let json_blob = new Blob([content], {type : "text/plain"});
+  let json_blob = new Blob([f_paragraph], {type : "text/plain"});
   let blob_url  = window.URL.createObjectURL(json_blob);
   window.location.assign(blob_url);
 }
