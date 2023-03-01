af6ec57ac9c7df77435fe7f23b5e1c14c7077849
Glitch (orbeez-2) <none>
2023-03-01 17:01:01 +0000

    💍🎀 Checkpoint ./constants.js:39955708/1117

--- constants.js
+++ constants.js
@@ -7,10 +7,16 @@ let constants = {
   restitution: 0.25,
   fov: 0.5,
   
+  //1 millimeter per frame squared is 3.6 meters per second squared
+  gravity: 2.725, //this is 1 g of acceleration
   
-  gravity: 2.75, //millimeters per frame squared = 3.6 meter per second squared
+  //the drag coefficient of a ball is 0.47
+  //an orbee weighs 1.4 grams
+  //and has a shadow area of 154 square millimetres
+  //air has a density of one gram per liter, or one micro-gram per cubic millimetre
+  //the square root of 2.725
   
-  
+  damping: .995
 }
 
 export {constants};
