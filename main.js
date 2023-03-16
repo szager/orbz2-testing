6daf1131f10dce6d526d06f4228fa13123acf5d4
Glitch (orbeez-2) <none>
2023-03-16 16:03:13 +0000

    🚈⛴ Checkpoint ./index.html:39955708/18 ./main.js:39955708/1292

--- main.js
+++ main.js
@@ -1,8 +1,17 @@
 import {game} from "./game.js";
 //import {obj2js} from "./obj2js.js";
+let game_canvas = document.createElement("canvas");
+if(confirm("How big is your screen?")) {
+  game_canvas.height = 720;
+  game_canvas.width = 1280;
+} else {
+  game_canvas.height = 540;
+  game_canvas.width = 960;
+}
 
+document.body.appendChild(game_canvas);
 const the_game = new game(
-  document.getElementById("game_canvas"),
+  game_canvas,
   document.getElementById("perf_canvas")
 );
 
