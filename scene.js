5250fcc4bb1adfb6b56f077c88f68be496265f5d
Glitch (orbeez-2) <none>
2023-04-04 16:25:56 +0000

    🏣🌏 Checkpoint ./object_3d.js:39955708/371 ./scene.js:39955708/1886

--- scene.js
+++ scene.js
@@ -11,41 +11,14 @@ class scene {
     this.min_distance = constants.near_distance;
     this.max_distance = constants.far_distance;
     
-    //alert(String(this.gl.getSupportedExtensions()));
-    
-    //this.extension_thing = this.gl.getExtension('ANGLE_instanced_arrays');
-    //if(!this.extension_thing) {
-     // alert("oh no your computer is bad :(");
-    //}
-    this.vertex_positions = [
-    ];
-    
     //let recipSqrt3 = 1 / Math.sqrt(3);
     
-    this.vertex_normals = [
-    ];
+    //this.vertex_normals = [
+    //];
     
-    this.object_indices = [
-    ];
-    
-    this.object_translations = [
-    ];
-    
-    this.objects = [
-      
-    ];
     this.objects = [];
     this.object_groups = [
       new group_3d(orbee_model, 80.0)
-    ];
-    
-    //this.object_colors = [
-    //];
-    
-    //this.object_shininess = [
-    //];
-    
-    this.faces = [
     ];
     
     
@@ -97,16 +70,12 @@ class scene {
       attribute_locations: {
         vertex_position: this.gl.getAttribLocation(this.shader_program, "vertex_position"),
         //vertex_normal: this.gl.getAttribLocation(this.shader_program, "vertex_normal"),
-        //object_index: this.gl.getAttribLocation(this.shader_program, "object_index"),
         color: this.gl.getAttribLocation(this.shader_program, "color"),
         position: this.gl.getAttribLocation(this.shader_program, "position"),
       },
       uniform_locations: {
         perspective_matrix: this.gl.getUniformLocation(this.shader_program, "perspective_matrix"),
         view_matrix: this.gl.getUniformLocation(this.shader_program, "view_matrix"),
-        //object_translations: this.gl.getUniformLocation(this.shader_program, "object_translations"),
-        //object_colors: this.gl.getUniformLocation(this.shader_program, "object_colors"),
-        //object_shininess: this.gl.getUniformLocation(this.shader_program, "object_shininess"),
         camera_translation: this.gl.getUniformLocation(this.shader_program, "camera_translation"),
       },
     };
@@ -124,7 +93,6 @@ class scene {
   }
 
   initialize_buffers() {
-    //this.float32_object_colors = new Float32Array(this.object_colors);
     this.object_groups.forEach(function(object_group) {
       
       object_group.vertex_position_buffer = this.gl.createBuffer();
@@ -177,12 +145,6 @@ class scene {
   }
   
   draw(time) {
-    //this.gl.useProgram(this.program_info.program);
-    //this.camera_rotation[0] = time * -.013357674575867674745;
-    //this.camera_rotation[1] = time * .0075564367798670867646;
-    //this.camera_rotation[2] = time * .0047658753564576776898;
-    //this.yaw = time * .186553465768875456;
-    //this.pitch = (Math.sin(time * 1.1654356656567) * 0.5 - 0.5);
     this.gl.clearColor(0.8, 0.8, 0.8, 1.0);
     this.gl.clearDepth(1.0);
     this.gl.enable(this.gl.DEPTH_TEST);
@@ -201,14 +163,6 @@ class scene {
     mat4.rotate(camera_matrix, camera_matrix, Math.PI / 2 - this.pitch, [1.0, 0.0, 0.0]);
     let view_matrix = mat4.create();
     mat4.invert(view_matrix, camera_matrix);
-    //this.gl.uniform3fv(
-      //this.program_info.uniform_locations.object_colors,
-      //this.float32_object_colors
-    //);
-    //this.gl.uniform1fv(
-      //this.program_info.uniform_locations.object_shininess,
-      //new Float32Array(this.object_shininess)
-    //);
     let camera_translation = [
       camera_matrix[8] * this.view_distance + this.focus[0],
       camera_matrix[9] * this.view_distance + this.focus[1],
@@ -219,10 +173,6 @@ class scene {
       let object_group = this.object_groups[i];
       this.draw_object_group(object_group, camera_translation, view_matrix, perspective_matrix);
     }
-    //this.object_groups.forEach(object_group => {
-      //this.draw_object_group(object_group);
-    //})
-    //alert(JSON.stringify(view_matrix, null, 1));
   }
   draw_object_group(object_group, camera_translation, view_matrix, perspective_matrix) {
     this.gl.uniform3f(
@@ -258,7 +208,7 @@ class scene {
     this.gl.bindBuffer(this.gl.ARRAY_BUFFER, object_group.color_buffer);
     this.gl.enableVertexAttribArray(this.program_info.attribute_locations.color);
     this.gl.vertexAttribPointer(this.program_info.attribute_locations.color, 3, this.gl.FLOAT, false, 0, 0);
-    //this.gl.vertexAttribDivisor(this.program_info.attribute_locations.color, 1);
+    this.gl.vertexAttribDivisor(this.program_info.attribute_locations.color, 1);
     
     
     //alert(String(object_group.positions));
@@ -266,12 +216,9 @@ class scene {
     this.gl.bindBuffer(this.gl.ARRAY_BUFFER, object_group.position_buffer);
     this.gl.enableVertexAttribArray(this.program_info.attribute_locations.position);
     this.gl.vertexAttribPointer(this.program_info.attribute_locations.position, 3, this.gl.FLOAT, false, 0, 0);
-    //this.gl.vertexAttribDivisor(this.program_info.attribute_locations.position, 1);
+    this.gl.vertexAttribDivisor(this.program_info.attribute_locations.position, 1);
     
     this.gl.bindBuffer(this.gl.ELEMENT_ARRAY_BUFFER, object_group.face_buffer);
-    
-    
-    
     //alert(String(object_group.position_buffer));
     
     this.gl.drawElementsInstanced(
