380c31be924c1dc59e4991c0680742956f84e25f
Glitch (orbz2-testing) <none>
2023-04-06 16:28:07 +0000

    🚴👲 Checkpoint ./some_example.js:39955708/1712

--- some_example.js
+++ some_example.js
@@ -3,13 +3,12 @@
 const vertexShaderSource = `#version 300 es
 in vec4 a_position;
 in vec4 color;
-in mat4 matrix;
 
 out vec4 v_color;
 
 void main() {
   // Multiply the position by the matrix.
-  gl_Position = matrix * a_position;
+  gl_Position = a_position;
 
   // Pass the vertex color to the fragment shader.
   v_color = color;
@@ -66,13 +65,13 @@ function main() {
 
   const positionLoc = gl.getAttribLocation(program, 'a_position');
   const colorLoc = gl.getAttribLocation(program, 'color');
-  const matrixLoc = gl.getAttribLocation(program, 'matrix');
+  //const matrixLoc = gl.getAttribLocation(program, 'matrix');
 
   // Create a vertex array object (attribute state)
-  const vao = gl.createVertexArray();
+  //const vao = gl.createVertexArray();
 
   // and make it the one we're currently working with
-  gl.bindVertexArray(vao);
+  //gl.bindVertexArray(vao);
 
   const positionBuffer = gl.createBuffer();
   gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
@@ -106,40 +105,40 @@ function main() {
   // setup matrixes, one per instance
   const numInstances = 5;
   // make a typed array with one view per matrix
-  const matrixData = new Float32Array(numInstances * 16);
-  const matrices = [];
-  for (let i = 0; i < numInstances; ++i) {
-    const byteOffsetToMatrix = i * 16 * 4;
-    const numFloatsForView = 16;
-    matrices.push(new Float32Array(
-        matrixData.buffer,
-        byteOffsetToMatrix,
-        numFloatsForView));
-  }
+  //const matrixData = new Float32Array(numInstances * 16);
+  //const matrices = [];
+//  for (let i = 0; i < numInstances; ++i) {
+    //const byteOffsetToMatrix = i * 16 * 4;
+  //  const numFloatsForView = 16;
+//    matrices.push(new Float32Array(
+      //  matrixData.buffer,
+    //    byteOffsetToMatrix,
+  //      numFloatsForView));
+//  }
 
-  const matrixBuffer = gl.createBuffer();
-  gl.bindBuffer(gl.ARRAY_BUFFER, matrixBuffer);
+ // const matrixBuffer = gl.createBuffer();
+//  gl.bindBuffer(gl.ARRAY_BUFFER, matrixBuffer);
   // just allocate the buffer
-  gl.bufferData(gl.ARRAY_BUFFER, matrixData.byteLength, gl.DYNAMIC_DRAW);
+ // gl.bufferData(gl.ARRAY_BUFFER, matrixData.byteLength, gl.DYNAMIC_DRAW);
 
   // set all 4 attributes for matrix
-  const bytesPerMatrix = 4 * 16;
-  for (let i = 0; i < 4; ++i) {
-    const loc = matrixLoc + i;
-    gl.enableVertexAttribArray(loc);
+//  const bytesPerMatrix = 4 * 16;
+//  for (let i = 0; i < 4; ++i) {
+ //   const loc = matrixLoc + i;
+  //  gl.enableVertexAttribArray(loc);
     // note the stride and offset
-    const offset = i * 16;  // 4 floats per row, 4 bytes per float
-    gl.vertexAttribPointer(
-        loc,              // location
-        4,                // size (num values to pull from buffer per iteration)
-        gl.FLOAT,         // type of data in buffer
-        false,            // normalize
-        bytesPerMatrix,   // stride, num bytes to advance to get to next set of values
-        offset,           // offset in buffer
-    );
+  //  const offset = i * 16;  // 4 floats per row, 4 bytes per float
+//    gl.vertexAttribPointer(
+    //    loc,              // location
+  //      4,                // size (num values to pull from buffer per iteration)
+//        gl.FLOAT,         // type of data in buffer
+       // false,            // normalize
+      //  bytesPerMatrix,   // stride, num bytes to advance to get to next set of values
+    //    offset,           // offset in buffer
+    //);
     // this line says this attribute only changes for each 1 instance
-    gl.vertexAttribDivisor(loc, 1);
-  }
+    //gl.vertexAttribDivisor(loc, 1);
+  //}
 
   // setup colors, one per instance
   const colorBuffer = gl.createBuffer();
@@ -164,24 +163,44 @@ function main() {
     time *= 0.001; // seconds
 
     //webglUtils.resizeCanvasToDisplaySize(gl.canvas);
-
+    
     // Tell WebGL how to convert from clip space to pixels
-    gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+    //gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
 
     gl.useProgram(program);
+    
+    
+    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
 
+  // setup the position attribute
+  gl.enableVertexAttribArray(positionLoc);
+  gl.vertexAttribPointer(
+      positionLoc,  // location
+      2,            // size (num values to pull from buffer per iteration)
+      gl.FLOAT,     // type of data in buffer
+      false,        // normalize
+      0,            // stride (0 = compute from size and type above)
+      0,            // offset in buffer
+  );
+    
+    
+    gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+    gl.enableVertexAttribArray(colorLoc);
+    gl.vertexAttribPointer(colorLoc, 4, gl.FLOAT, false, 0, 0);
+    // this line says this attribute only changes for each 1 instance
+    gl.vertexAttribDivisor(colorLoc, 1);
     // setup all attributes
-    gl.bindVertexArray(vao);
+    //gl.bindVertexArray(vao);
 
     // update all the matrices
-    matrices.forEach((mat, ndx) => {
+    //matrices.forEach((mat, ndx) => {
       //mat4.translation(-0.5 + ndx * 0.25, 0, 0, mat);
       //mat4.zRotate(mat, time * (0.1 + 0.1 * ndx), mat);
-    });
+    //});
 
     // upload the new matrix data
-    gl.bindBuffer(gl.ARRAY_BUFFER, matrixBuffer);
-    gl.bufferSubData(gl.ARRAY_BUFFER, 0, matrixData);
+    //gl.bindBuffer(gl.ARRAY_BUFFER, matrixBuffer);
+    //gl.bufferSubData(gl.ARRAY_BUFFER, 0, matrixData);
 
     gl.drawArraysInstanced(
       gl.TRIANGLES,
