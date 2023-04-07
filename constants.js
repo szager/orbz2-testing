3fe85f4f96ba1759d9c6c7a5d16b0a0e1dbf26cd
Glitch (orbz2-testing) <none>
2023-04-07 17:35:55 +0000

    📺🍃 Checkpoint ./models.js:39955708/1501 ./constants.js:39955708/28

--- constants.js
+++ constants.js
@@ -5,7 +5,7 @@ let constants = {
   near_distance: 10,
   traction: 0.01,
   restitution: 0.25,
-  sensitivity: 0.005, //pixels per radian
+  sensitivity: 0.002, //radians per pixel
   fov: 0.5,
   frame_timestamps: 90,
   
