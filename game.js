3638f147c7e815475d89cb5fab585e1d4a315925
Glitch (orbz2-testing) <none>
2023-04-27 21:13:43 +0000

    ⛱🚂 Checkpoint ./game.js:39955708/511

--- game.js
+++ game.js
@@ -156,9 +156,9 @@ class game {
   update() {
     this.time += this.frame_time;
     
-    this.scene.objects[4].position[0] += Math.sin(this.time * 5.186542) + Math.sin(this.time * 6.569);
-    this.scene.objects[4].position[1] +=
-      this.scene.objects[4].position[2] += Math.sin(this.time * 4.6358873);
+
+    this.scene.objects[4].position[1] += Math.cos(this.time * 4.6358873) + Math.sin(this.time * 11.63375) - Math.sin(this.time * 12.237126) + Math.cos(this.time * 9.86430864);
+      this.scene.objects[4].position[2] += Math.sin(this.time * 4.6358873) + Math.cos(this.time * 11.63375) - Math.cos(this.time * 12.237126) + Math.sin(this.time * );
     //let amount_of_useless_math = 2**(Math.random() * 24);
     //for(let i = 0; i < amount_of_useless_math; i++) {
       //let e = Math.sin(Math.random() / 7673.6587988);
