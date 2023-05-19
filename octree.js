0b205b98bdd897009da89d2b0b563ccb74f10be4
Glitch (orbz2-testing) <none>
2023-05-19 18:18:18 +0000

    🚲💕 Checkpoint ./octree.js:39955708/734

--- octree.js
+++ octree.js
@@ -1,5 +1,16 @@
 class octree { //it's O(nlogn)
   constructor(orbeez) {
     this.orbeez_inside = orbeez;
+    this.corner_a = [-512, -512, -512];
+    this.corner_b = [512, 512, 512];
+  }
+}
+
+class octree_branch {
+  constructor(corner_a, corner_b, parent, depth) {
+    this.parent = parent;
+    this.corner_a = corner_a;
+    this.corner_b = corner_b;
+    
   }
 }
