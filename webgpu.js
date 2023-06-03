bae2c1c6d2f9e63eeb08ee142fe12ab3adf80861
szager@gmail.com <szager@gmail.com>
2023-06-03 19:35:35 +0000

    Fix binding

--- webgpu.js
+++ webgpu.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
   if (navigator && navigator.gpu) {
     let old_orbee_interactions = the_game.__proto__.orbee_interactions;
     the_game.__proto__.orbee_interactions = () => {
-      old_orbee_interactions.bind(this)();
+      old_orbee_interactions.bind(the_game)();
     };
   }
 });
